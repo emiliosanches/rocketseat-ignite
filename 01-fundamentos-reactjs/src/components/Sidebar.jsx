9e3d24d372dece24fa6ff0ad91ca9fe4da4c3848
Emilio Sanches <emilio.neto.32123@gmail.com>
2022-11-27 19:02:18 -0300

    IgniteFeed: Create Avatar component to avoid code repetition

--- 01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ 01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from "phosphor-react";
+import { Avatar } from "./Avatar";
 import styles from "./Sidebar.module.css";
 
 export function Sidebar() {
@@ -10,10 +11,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://github.com/emiliosanches.png"
-        />
+        <Avatar src="https://github.com/emiliosanches.png" />
 
         <strong>Emilio Sanches</strong>
         <span>Full-Stack Developer</span>
