05a88510e7aaae7cfb263770c74d493512d148ef
Emilio Sanches <emilio.neto.32123@gmail.com>
2022-12-01 12:07:11 -0300

    IgniteTimer: Use styled-components theme provider

--- 02-ignite-timer/src/components/Button/styles.ts
+++ 02-ignite-timer/src/components/Button/styles.ts
@@ -5,15 +5,15 @@ interface ButtonContainerProps {
   variant: ButtonVariant;
 }
 
-const buttonVariantsColors = {
-  primary: 'purple',
-  secondary: 'orange',
-  danger: 'red',
-  success: 'green',
-}
+// const buttonVariantsColors = {
+//   primary: 'purple',
+//   secondary: 'orange',
+//   danger: 'red',
+//   success: 'green',
+// }
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 400px;
   height: 100px;
-  background: ${(props) => buttonVariantsColors[props.variant]};
+  background: ${(props) => props.theme[props.variant]};
 `;
